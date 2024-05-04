b8f1eafe977c80e04b56faf6c6df9c012cacc8a8
Daniel Newton <kdaniel.newton@gmail.com>
2024-05-04 16:42:27 -0500

    Implement socket.io server

--- apps/web-sockets/src/main.ts
+++ apps/web-sockets/src/main.ts
@@ -1,21 +1,29 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import http from 'http';
 import express from 'express';
-import * as path from 'path';
+import { Server } from 'socket.io';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send({ message: 'Welcome to web-sockets!' });
 });
 
+const server = http.createServer(app);
 const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
 });
-server.on('error', console.error);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
