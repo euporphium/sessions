58b18b5ddc91dc0f19fa52b36eb6dfa0f7eec91f
Daniel Newton <kdaniel.newton@gmail.com>
2024-05-24 21:56:01 -0500

    Update home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+
 export default async function Index() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="grid min-h-dvh place-items-center">
+      <div className="flex flex-1 flex-col items-center">
+        <h1 className="text-4xl font-medium">Welcome</h1>
+
+        <Link className="mt-10 underline" href={{ pathname: '/session' }}>
+          Create Session
+        </Link>
+      </div>
+    </main>
   );
 }
