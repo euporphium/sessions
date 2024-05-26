5f872de4d1152e96205da29e24d2e13192314a74
Daniel Newton <kdaniel.newton@gmail.com>
2024-05-25 20:54:49 -0500

    Update session page

--- apps/web/src/app/session/[slug]/page.tsx
+++ apps/web/src/app/session/[slug]/page.tsx
@@ -1,7 +1,20 @@
+import { getAuthenticatedUser, getSessionBySlug } from '../actions';
+import { notFound, redirect } from 'next/navigation';
+
 type SessionPageProps = {
   params: { slug: string };
 };
 
-export default function SessionPage({ params }: SessionPageProps) {
-  return <div>Welcome to your Session!</div>;
+export default async function SessionPage({ params }: SessionPageProps) {
+  const user = await getAuthenticatedUser();
+  if (!user) {
+    redirect('/api/auth/login');
+  }
+
+  const session = await getSessionBySlug(params.slug);
+  if (!session) {
+    notFound();
+  }
+
+  return <div>Welcome to {session.name}</div>;
 }
